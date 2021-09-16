8f2c86b79cc7e438d4406f2e2446e8d717df6267
Christopher Kapic <christopherkapic@gmail.com>
2021-09-16 16:34:36 -0600

    home page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,19 @@ import Navbar from '../components/Navbar'
 import profilePic from '../public/prof_neon.png'
 import styles from '../styles/Home.module.css'
 
+import Particles from "react-tsparticles";
+
+import react_logo from '../public/tools/react.png'
+import node_logo from '../public/tools/nodejs.png'
+import typescript_logo from '../public/tools/typescript.png'
+import python_logo from '../public/tools/python.png'
+import postgres_logo from '../public/tools/postgres.png'
+import redis_logo from '../public/tools/redis.svg'
+import netlify_logo from '../public/tools/netlify.png'
+import firebase_logo from '../public/tools/firebase.png'
+import supabase_logo from '../public/tools/supabase.png'
+import vultr_logo from '../public/tools/vultr.png'
+
 
 
 const Home: NextPage = () => {
@@ -19,12 +32,55 @@ const Home: NextPage = () => {
 
       <div className={styles.hero}> {/* Hero Section */}
         <div style={{width: "100%", height: 300, display: "flex", justifyContent: "center"}}>
-          <Image src={profilePic} alt="Picture of Christopher Kapic" height={300} width={300}/>
+          <div style={{width: 300, height: 300, borderRadius: 150, zIndex: 2}}>
+            <Image src={profilePic} alt="Picture of Christopher Kapic" height={300} width={300}/>
+          </div>
         </div>
-        <h1>Christopher Kapic</h1>
-        <h4>Web Developer • Student • Drone Pilot</h4>
-        <p>Hi, I'm Christopher, a student at Northwestern University ('21, graduating in December). To get to know me, I encourage you to explore my website or check out my <a href="https://blog.kapic.io">blog</a>.</p>
+        <h1 style={{zIndex: 2}}>Christopher Kapic</h1>
+        <h4 style={{zIndex: 2}}>Web Developer • Student • Drone Pilot</h4>
+        <p style={{zIndex: 2}}>Hi, I'm Christopher, a student at Northwestern University ('21, graduating in December). To get to know me, I encourage you to explore my website or check out my <a href="https://blog.kapic.io">blog</a>.</p>
+        <h5 style={{zIndex: 2}}>My toolset:</h5>
+        <div className={styles.toolset}>
+          <div className={styles.tool}>
+            <Image src={react_logo} alt="React" layout="fixed" height={108} width={121}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={node_logo} alt="React" layout="fixed" height={108} width={98}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={typescript_logo} alt="React" layout="fixed" height={108} width={108}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={python_logo} alt="React" layout="fixed" height={108} width={108}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={postgres_logo} alt="React" layout="fixed" height={108} width={108}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={redis_logo} alt="React" layout="fixed" height={108} width={126}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={netlify_logo} alt="React" layout="fixed" height={108} width={108}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={firebase_logo} alt="React" layout="fixed" height={108} width={79}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={supabase_logo} alt="React" layout="fixed" height={108} width={108}/>
+          </div>
+          <div className={styles.tool}>
+            <Image src={vultr_logo} alt="React" layout="fixed" height={108} width={108}/>
+          </div>
+        </div>
       </div>
+      <Particles 
+        id="tsparticles"
+        url="/particles.json"
+        init={() => console.log('hi')}
+        loaded={() => console.log('hi2')}
+        className={styles.particleswrapper}
+        canvasClassName={styles.particles}
+      />
 
       
     </div>
