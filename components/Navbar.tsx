cfc7b4cd72a79bdc46b34f2b9bf6d481e1e07b1d
Christopher Kapic <christopherkapic@gmail.com>
2021-09-16 14:58:51 -0600

    particles.js testing next

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,20 @@
 import { Navbar as NavbarBS, Container, Nav, NavDropdown } from 'react-bootstrap'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
         <NavbarBS bg="dark" expand="lg" variant="dark">
         <Container>
-          <NavbarBS.Brand href="#">Christopher Kapic</NavbarBS.Brand>
+          <Link href="/">
+            <NavbarBS.Brand href="/">Christopher Kapic</NavbarBS.Brand>
+          </Link>
           <NavbarBS.Toggle aria-controls="basic-navbar-nav" />
           <NavbarBS.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="http://blog.kapic.io">Blog</Nav.Link>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+              <Link href="/portfolio">
+                <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              </Link>
               <NavDropdown title="Socials" id="basic-nav-dropdown">
                 <NavDropdown.Item href="https://github.com/christopher-kapic">Github</NavDropdown.Item>
                 <NavDropdown.Item href="https://linkedin.com/in/christopher-kapic">LinkedIn</NavDropdown.Item>
