c66788352096adcea63fac84bc52fb44050dbd91
Christopher Kapic <christopherkapic@gmail.com>
2021-09-17 08:27:59 -0600

    working on portfolio, removed Portfolio in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,9 +12,9 @@ const Navbar = () => {
           <NavbarBS.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="http://blog.kapic.io">Blog</Nav.Link>
-              <Link href="/portfolio">
+              {/* <Link href="/portfolio">
                 <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-              </Link>
+              </Link> */}
               <NavDropdown title="Socials" id="basic-nav-dropdown">
                 <NavDropdown.Item href="https://github.com/christopher-kapic">Github</NavDropdown.Item>
                 <NavDropdown.Item href="https://linkedin.com/in/christopher-kapic">LinkedIn</NavDropdown.Item>
