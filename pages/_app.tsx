b64c6a491eec184848cb408cceb2d5c502247d30
Christopher Kapic <christopherkapic@gmail.com>
2021-12-14 08:31:30 -0600

    new

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
